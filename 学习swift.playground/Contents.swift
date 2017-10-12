c4502987cd5eb69492309674e83b76c7b77c817d
李招阳 <lizhaoyang@lizhaoyang.local>
2017-10-12 16:20:04 +0800

    the first

---
+++ 学习swift.playground/Contents.swift
@@ -0,0 +1,68 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//let a=1
+let b:Int = 12
+var c:Double=3.1415926535857
+var d = 2
+let num:Int=14
+var yuanzu=(1,4)
+let imtrue=true;
+let bool1=false
+if imtrue {
+    print("im'true")
+}
+else {
+    print ("im'false")
+}
+var int=(1,2.13,"haha")
+
+var (yi,er,san)=int
+yi
+
+er
+let point = (x:1,y:3)
+point.x
+point.y
+var point1:(Double,Double)=(3,2)
+print("")
+
+
+var imstring="hehe"
+print("我的名字是" + imstring)
+print("😂")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
